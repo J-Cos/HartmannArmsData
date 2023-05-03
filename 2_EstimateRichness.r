88d2e05ea93d4c3606c6c4444badab98f1f3e9d0
J <j.williams20@imperial.ac.uk>
2023-05-03 16:40:23 +0100

    Add ESV endemics quantification

--- 2_EstimateRichness.r
+++ 2_EstimateRichness.r
@@ -36,6 +36,53 @@
         )
     }
 
+GetRarefiedReplicatePsList<-function(psList=ps_l, Depth=Depths, Gene, NumberOfReplicates){
+
+        smallestsamples<-ps_l[[Gene]] %>%sample_sums %>% sort %>% head
+
+
+        if (as.vector(smallestsamples[1])==(Depth[[Gene]]+1)) {
+            ps_rarefiedReplicate_l<-list()
+            for (i in 1:NumberOfReplicates) {
+                        ps_rarefiedReplicate_l[[i]]<-ps_l[[Gene]] %>%
+                            rarefy_even_depth(  physeq=., sample.size = Depth[[Gene]], rngseed = i, replace = TRUE, trimOTUs = TRUE, verbose = TRUE) 
+            }
+        } else{
+            print(paste0("Warning: ", Gene, " data has changed"))
+        }
+
+        return(ps_rarefiedReplicate_l)
+}
+    
+
+GetEndemicsDataframe<-function(psList){
+    df_list<-list()
+    for (i in 1:length(psList)){
+
+        ps<-psList[[i]]
+        df_list[[i]]<-data.frame(row.names=sample_names(ps),ARMS=sample_data(ps)$ARMS, NumberEndemics=NA, Replicate=i)
+
+
+        for (sample in sample_names(ps)) {
+            
+            ESVsPresentInSample<- taxa_names(ps)    [(otu_table(ps)[,sample]>1)]
+            
+             df_list[[i]][sample,"NumberEndemics"]<-ps %>%
+                prune_taxa(ESVsPresentInSample, .) %>%
+                prune_samples(!sample_names(ps) %in% sample, .) %>%
+                taxa_sums %>%
+                `<`(1) %>%
+                sum
+        }
+    }
+
+    df<-bind_rows(df_list) %>%
+        as_tibble %>%
+        pivot_wider(names_from=Replicate, values_from=NumberEndemics)
+
+    return(df)
+}
+
 #1) load data
     ps_l<-readRDS(file.path("Outputs", "AaronsARMS.RDS"))
 
@@ -61,3 +108,32 @@
     write.csv(RichnessStats, file.path("Outputs", "RichnessStats.csv"))
     write.csv(RichnessCoiDf_list[["Replicates"]], file.path("Outputs", "COIRarefyReplicates.csv"))
     write.csv(Richness16sDf_list[["Replicates"]], file.path("Outputs", "16sRarefyReplicates.csv"))
+
+# 4) get endemics ad output to csv
+    psCOI_rarefiedReplicate_l<-GetRarefiedReplicatePsList(psList=ps_l, Depth=Depths, Gene="COI", NumberOfReplicates=Replicates)
+    EndemicCOIDf<-GetEndemicsDataframe(psCOI_rarefiedReplicate_l)
+    ps16_rarefiedReplicate_l<-GetRarefiedReplicatePsList(psList=ps_l, Depth=Depths, Gene="16s", NumberOfReplicates=Replicates)
+    Endemics16Df<-GetEndemicsDataframe(ps16_rarefiedReplicate_l)
+
+    EndemicCOIStats<-EndemicCOIDf %>%
+            rowwise() %>%
+            mutate(
+                mean = mean(c_across(where(is.numeric))),
+                SD = sd(c_across(where(is.numeric))),
+                SE = SD/Replicates) %>%
+            select(ARMS, mean, SD, SE)
+
+    Endemic16Stats<-Endemics16Df %>%
+            rowwise() %>%
+            mutate(
+                mean = mean(c_across(where(is.numeric))),
+                SD = sd(c_across(where(is.numeric))),
+                SE = SD/Replicates) %>%
+            select(ARMS, mean, SD, SE)
+
+    EndemicityStats<-full_join(EndemicCOIStats,Endemic16Stats, by="ARMS", suffix=c("_COI", "_16s")) %>%
+        mutate(NumberRarefyReplicates=Replicates)
+    
+    write.csv(EndemicityStats, file.path("Outputs", "EndemicityStats.csv"))
+    write.csv(EndemicCOIDf, file.path("Outputs", "endemics_COIRarefyReplicates.csv"))
+    write.csv(Endemics16Df, file.path("Outputs", "endemics_16sRarefyReplicates.csv"))
